17e37c309792c87f441679651defeea6e9863cf0
dragos-dev <dragos.tzx@gmail.com>
2022-12-25 05:40:58 +0300

    feat: read line and store

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,32 @@
+extern crate core;
+
+use core::panicking::panic;
+use std::io;
+
 struct Rectangle {
     width: u32,
     height: u32,
 }
 
 fn main() {
-    println!("Hello, world!");
+    let mut height = String::new();
+    let mut width = String::new();
+
+    io::stdin()
+        .read_line(&mut width)
+        .expect("Invalid input");
+
+    io::stdin()
+        .read_line(&mut height)
+        .expect("Invalid input");
+
+    let width: u32 = match width.parse() {
+        Ok(x) => x,
+        Err(err) => panic!("Invalid height: {}", err)
+    };
+
+    let height: u32 = match height.parse() {
+        Ok(x) => x,
+        Err(err) => panic!("Invalid height: {}", err)
+    };
 }
