1e627651db18c4ec486e7263de5122635adb9ccd
dragos-dev <dragos.tzx@gmail.com>
2022-12-25 05:50:06 +0300

    feat: function to calculate area and print it

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 extern crate core;
 
-use core::panicking::panic;
 use std::io;
 
 struct Rectangle {
@@ -12,21 +11,40 @@ fn main() {
     let mut height = String::new();
     let mut width = String::new();
 
+    println!("Input width: ");
     io::stdin()
         .read_line(&mut width)
         .expect("Invalid input");
 
+    println!("Input height: ");
     io::stdin()
         .read_line(&mut height)
         .expect("Invalid input");
 
-    let width: u32 = match width.parse() {
+    let width: u32 = match width.trim().parse() {
         Ok(x) => x,
-        Err(err) => panic!("Invalid height: {}", err)
+        Err(err) => {
+            println!("Error: {}", err);
+            return
+        }
     };
 
-    let height: u32 = match height.parse() {
+    let height: u32 = match height.trim().parse() {
         Ok(x) => x,
-        Err(err) => panic!("Invalid height: {}", err)
+        Err(err) => {
+            println!("Error: {}", err);
+            return
+        }
     };
+
+    let rectangle: Rectangle = Rectangle {
+        width,
+        height
+    };
+
+    println!("Rectangle area is: {}", area(&rectangle));
+}
+
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
